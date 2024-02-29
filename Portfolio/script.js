4a2ab63cd87f2a56fc9ef632ce067d8d2cbbeea2
Can Dinler <candinler4@gmail.com>
2024-03-01 02:26:27 +0300

    Portfolio
    Portfolio

---
+++ Portfolio/script.js
@@ -0,0 +1,16 @@
+const navBtn = document.querySelector('.navBtn');
+const sidebar = document.querySelector('#sidebar');
+const closeBtn = document.querySelector('#closeBtn');
+const sidebarLinks = document.querySelector('.sidebarLinks');
+
+navBtn.addEventListener('click', () => {
+  sidebar.classList.add('showSidebar');
+});
+
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('showSidebar');
+});
+
+sidebarLinks.addEventListener('click', () => {
+  sidebar.classList.remove('showSidebar');
+});
